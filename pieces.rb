8e16c721b718fa62256e639bc48fd9ddea2acb40
Your Name <josefdaly@gmail.com>
2015-06-23 17:46:57 -0700

    Gemify

--- pieces.rb
+++
@@ -1,6 +0,0 @@
-require_relative 'pieces/king.rb'
-require_relative 'pieces/knight.rb'
-require_relative 'pieces/pawn.rb'
-require_relative 'pieces/queen.rb'
-require_relative 'pieces/rook.rb'
-require_relative 'pieces/bishop.rb'
